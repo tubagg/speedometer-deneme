f66a3d2d8c720297d6e1d120d4490afafcee8448
Tuğba Bozkurt <tubzkrt@gmail.com>
2023-02-08 12:13:02 +0300

    update

--- speed.js
+++ speed.js
@@ -1,5 +1,5 @@
 var speed = 0;
-var prevspeed = 0;
+var prevSpeed = 0;
 var currentScale = 1;
 
 function increaseSpeed(){
@@ -8,6 +8,7 @@ function increaseSpeed(){
     addClass();
     currentScale = currentScale + 1;
     changeActive();
+    changeText();
 }
 }
 
@@ -15,25 +16,33 @@ function decreaseSpeed(){
      if (speed > 0){            //hız değişkeni 0'dan büyükse,hız değişkenini 10 azaltır ve addClass fonksiyonunu çağırır.
         speed = speed - 10;
     addClass();
-    currentScale = currentScale - 1;
     changeActive();
+    currentScale = currentScale - 1;
+    changeText();
+    
 }
 }
 
 function addClass(){ //hız değişkenine dayalı olarak yeni bir sınıf oluşturur ve eski sınıfı kaldırarak bu sınıfı uygular.
     let newClass = "speed-" + speed;
-    let prevClass = "speed-" + prevspeed;
+    let prevClass = "speed-" + prevSpeed;
     let el = document.getElementsByClassName("arrow-wrapper")[0];
     if (el.classList.contains(prevClass)){
         el.classList.remove(prevClass);
         el.classList.add(newClass);
 
     }
-    prevspeed = speed;
+    prevSpeed = speed;
 
 }
 function changeActive(){
     let tempClass = "speedometer-scale-" + currentScale;
     let el = document.getElementsByClassName(tempClass)[0];
     el.classList.toggle("active");
+}
+
+function changeText(){
+    let el = document.getElementsByClassName("km")[0];
+    el.innerText = speed;
+
 }
